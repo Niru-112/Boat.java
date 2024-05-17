a0c3f65b8028ac8d62592a4be32f8ce54d175f6d
Niru-112 <155888527+Niru-112@users.noreply.github.com>
2024-05-17 18:39:34 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Product {
+    private String name;
+    private double price;
+    private int quantity;
+  
+    public Product(String name, double price, int quantity) {
+      this.name = name;
+      this.price = price;
+      this.quantity = quantity;
+    }
+  
+    public String getName() {
+      return name;
+    }
+  
+    public double getPrice() {
+      return price;
+    }
+  
+    public int getQuantity() {
+      return quantity;
+    }
+  }
 class ShopProduct
 {
     public ShopProduct()
